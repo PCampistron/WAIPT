fe9eb92bb6f09e4cf621d505851b5273af5edbe1
Pierre Campistron <pier.campistron@gmail.com>
2023-01-16 21:56:40 +0100

    Script JS pour choix critères

--- src/index.php
+++ src/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" style="text/css" href="style.css" />
+    <script src="script.js"></script>
     <title>WAIPT</title>
 
     
@@ -48,31 +49,31 @@
         <input type="text" name="query"/>
 
         <p>Premier critère :</p>
-        <input type="radio" name="crit1" value="dateSortie">
+        <input type="radio" name="crit1" onmouseup="activerCrit2()" value="dateSortie">
         <label for="dateSortie">Date de sortie</label><br>
-        <input type="radio" name="crit1" value="nbUtilisateur">
+        <input type="radio" name="crit1" onmouseup="activerCrit2()"  value="nbUtilisateur">
         <label for="nbUtilisateur">Nombre d'utilisateurs</label><br>
-        <input type="radio" name="crit1" value="nbDefi">
+        <input type="radio" name="crit1" onmouseup="activerCrit2()"  value="nbDefi">
         <label for="nbDefi">Nombre de défis</label>
 
         <br>  
 
         <p>Second critère :</p>
-        <input type="radio" name="crit2" value="dateSortie">
+        <input type="radio" name="crit2" id="bouton4" onmouseup="activerCrit3()" value="dateSortie" disabled>
         <label for="dateSortie">Date de sortie</label><br>
-        <input type="radio" name="crit2" value="nbUtilisateur">
+        <input type="radio" name="crit2" id="bouton5" onmouseup="activerCrit3()" value="nbUtilisateur" disabled>
         <label for="nbUtilisateur">Nombre d'utilisateurs</label><br>
-        <input type="radio" name="crit2" value="nbDefi">
+        <input type="radio" name="crit2" id="bouton6" onmouseup="activerCrit3()" value="nbDefi" disabled>
         <label for="nbDefi">Nombre de défis</label>
 
         <br>  
 
         <p>Troisième critère :</p>
-        <input type="radio" name="crit3" value="dateSortie">
+        <input type="radio" name="crit3" id="bouton7" value="dateSortie" disabled>
         <label for="dateSortie">Date de sortie</label><br>
-        <input type="radio" name="crit3" value="nbUtilisateur">
+        <input type="radio" name="crit3" id="bouton8" value="nbUtilisateur" disabled>
         <label for="nbUtilisateur">Nombre d'utilisateurs</label><br>
-        <input type="radio" name="crit3" value="nbDefi">
+        <input type="radio" name="crit3" id="bouton9" value="nbDefi" disabled>
         <label for="nbDefi">Nombre de défis</label>
 
         <br><br><br>
---
+++ src/script.js
@@ -0,0 +1,13 @@
+function activerCrit2()
+{
+    document.getElementById("bouton4").disabled = false;
+    document.getElementById("bouton5").disabled = false;
+    document.getElementById("bouton6").disabled = false;
+}
+
+function activerCrit3()
+{
+    document.getElementById("bouton7").disabled = false;
+    document.getElementById("bouton8").disabled = false;
+    document.getElementById("bouton9").disabled = false;
+}
