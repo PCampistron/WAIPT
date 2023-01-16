dbbb4a7b9b10910963ecd9b5fce53cdf73108ebc
Pierre Campistron <pier.campistron@gmail.com>
2023-01-16 14:29:12 +0100

    indentation classement.php

--- src/classement.php
+++ src/classement.php
@@ -1,22 +1,23 @@
 <?php
-    include "connDb.php" ; // Connexion à la BD
+   include "connDb.php" ; // Connexion à la BD
 
 
-    // preparer la requete 
- $requete = $db -> prepare ("SELECT * FROM JEU ORDER BY reputation DESC LIMIT 10;");
-    // executer la requête
- $requete -> execute();
- $results= $requete->fetchAll();  // organiser les résultats de la requête
+   // preparer la requete 
+   $requete = $db -> prepare ("SELECT * FROM JEU ORDER BY reputation DESC LIMIT 10;");
+
+   // executer la requête
+   $requete -> execute();
+   $results= $requete->fetchAll();  // organiser les résultats de la requête
  
- $i = 0;
- foreach ($results as $row) {
-    $i++;
-    echo '<article class="caseJeu">';
-    // affciher le rang du jeu
-    echo '<h3> ' . $i . ' :' . $row['nom'] . ' </h3>' ;
-    //afficher l'image
-    echo '<img src="img/'.$row['id_jeu'].'_carre.jpg" height="20%" width="10%" alt="Pochette">';
-    echo '</article>';
+   $i = 0;
 
- }
+   foreach ($results as $row) {
+      $i++;
+      echo '<article class="caseJeu">';
+      // affciher le rang du jeu
+      echo '<h3> ' . $i . ' :' . $row['nom'] . ' </h3>' ;
+      //afficher l'image
+      echo '<img src="img/'.$row['id_jeu'].'_carre.jpg" height="20%" width="10%" alt="Pochette">';
+      echo '</article>';
+   }
 ?>
