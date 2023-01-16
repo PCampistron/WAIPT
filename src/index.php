577ded43fbc8b4578567af513513606bec114984
Esteban Labastie <elabastie@iutbayonne.univ-pau.fr>
2023-01-16 16:50:39 +0100

    Ajout du css

--- src/index.php
+++ src/index.php
@@ -4,12 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" style="text/css" href="style.css" />
     <title>WAIPT</title>
+
+    
+    <header>
+    <div id="gauche">
+        <a href="index.php">
+          <img src="img/Logo.jpg" alt="logo" height=50px width=50px>
+        </a>
+         <titre> What Am I Playing Today ? </titre>
+      </div>
+      <div id="centre">
+        <nav>
+          <ul>
+            <li>
+              <a href="catalogue.php">
+                Catalogue
+              </a>
+            </li>
+            <li>
+              <a href="classement.php">
+                Classement
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="droite">
+        <a href="#">
+          <p>Se connecter</p>
+        </a>
+      </div>
+
+  </header>
+  <main>
+    <div class="container">
+      <h1>Rechercher un jeu</h1>
+
 </head>
 <body>
-
-
-    <form action="CalculReput.php" method="POST">
+    <form action="recherche.php" method="POST">
         <input type="text" name="query"/>
 
         <p>Premier critère :</p>
@@ -40,7 +75,7 @@
         <input type="radio" name="crit3" value="nbDefi">
         <label for="nbDefi">Nombre de défis</label>
 
-        <br>
+        <br><br><br>
 
         <input type="submit" value="Rechercher">
     </form>
