d3ece4251c7b4c6c610968757373d331546e2a0b
Pierre Campistron <pier.campistron@gmail.com>
2023-02-06 15:54:53 +0100

    Documentation de la classe TraiterBD

--- src/TraiterBD.php
+++ src/TraiterBD.php
@@ -1,4 +1,13 @@
 <?php
+    /** 
+     * @file TraiterBD.php
+     * @author pcampistron
+     * @brief Spécifie une classe TraiterBD utile au traitement de la base de données.
+     * @details Gères différentes opérations comme la connexion ou la mise à jour de la base de données.
+     * @version 0.1
+     * @date 03/02/2023
+     */
+
     include "Requete.php";
 
     class TraiterBD
@@ -7,12 +16,22 @@
         private Requete $requete;
         
         // CONSTRUCTEUR
+
+        /**
+         * @brief Constructeur de la classe TraiterBD
+         * @details Constructeur de la classe TraiterBD prenant en paramètre uneRequete correspondant à la requête de recherche formulée par l'utilisateur
+         * @param [in] uneRequete Requete contenant différentes informations concernant la recherche de l'utilisateur
+         */
         public function __construct(Requete $uneRequete)
         {
             $this->requete = $uneRequete;
         }
 
         // METHODES SPECIFIQUES
+
+        /**
+         * @brief Met à jour les données de la BD à partir des données présentes dans requete
+         */
         public function majBD()
         {
             $dbname ='pcampistron_bd';
