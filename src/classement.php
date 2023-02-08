c82c306c0b7f456d185fcda7262fc0c7014cb10c
dsokhna <daddydjim20@gmail.com>
2023-02-08 14:13:26 +0100

    Changement de la barre de navigation en utilisant bootstrap

--- src/classement.php
+++ src/classement.php
@@ -4,38 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" style="text/css" href="style.css" />
     <title>WAIPT</title>
 
     
-    <header>
-    <div id="gauche">
-        <a href="index.php">
-          <img src="img/Logo.jpg" alt="logo" height=50px width=50px> 
-        </a>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+          <img src="img/Logo.jpg" alt="Logo" height=50px width=50px class="d-inline-block align-text-top">
+          What Am I Playing Today ?
+          </a>
+        </div>
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="navbar-brand" href="index.php">ACCUEIL</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        </li>
       </div>
-      <div id="centre">
-        <nav>
-          <ul>
-            <li>
-              <a href="catalogue.php">
-                Catalogue
-              </a>
-            </li>
-            <li>
-              <a href="classement.php">
-                Classement
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="navbar-brand" href="catalogue.php">CATALOGUE</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </li>
+        </ul>
       </div>
-      <div id="droite">
-        <a href="#">
-          <p>Se connecter</p>
-        </a>
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="navbar-brand" href="classement.php">CLASSEMENT</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </li>
+        </ul>
+        <button class="btn btn-outline-success" type="submit">Se connecter</button>
       </div>
-
+    </nav>
   </header>
   <main>
     <div class="container">
--- src/index.php
+++ src/index.php
@@ -4,49 +4,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" style="text/css" href="style.css" />
     <script src="script.js"></script>
+    
     <title>WAIPT</title>
 
-    
-    <header>
-    <div id="gauche">
-        <a href="index.php">
-          <img src="img/Logo.jpg" alt="logo" height=50px width=50px>
-        </a>
-         <titre> What Am I Playing Today ? </titre>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+          <img src="img/Logo.jpg" alt="Logo" height=50px width=50px class="d-inline-block align-text-top">
+          What Am I Playing Today ?
+          </a>
+        </div>
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="navbar-brand" href="index.php">ACCUEIL</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        </li>
       </div>
-      <div id="centre">
-        <nav>
-          <ul>
-            <li>
-              <a href="catalogue.php">
-                Catalogue
-              </a>
-            </li>
-            <li>
-              <a href="classement.php">
-                Classement
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="navbar-brand" href="catalogue.php">CATALOGUE</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </li>
+        </ul>
       </div>
-      <div id="droite">
-     <form  action="connexion.php" method="post">
-    <input type="submit" value="Se connecter">
-    </form>
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="navbar-brand" href="classement.php">CLASSEMENT</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </li>
+        </ul>
+        <button class="btn btn-outline-success" type="submit">Se connecter</button>
       </div>
-
+    </nav>
   </header>
-  <main>
-    <div class="container">
-      <h1>Rechercher un jeu</h1>
+    
 
 </head>
 <body>
     <form action="recherche.php" method="POST">
-        <input type="text" name="query"/>
+      <div class="container">
+        <img src="" class="img-fluid" alt="...">
+        <div class="text">
+          <h1>Tirer un jeu au hasard</h1>
+        </div>
+      </div>
+
+    
+      <!--  
+      <input type="text" name="query"/>
 
         <p>Premier critère :</p>
         <input type="radio" name="crit1" onmouseup="activerCrit2()" value="dateSortie">
@@ -80,9 +100,7 @@
 
         <input type="submit" value="Rechercher">
     </form>
-
-
-    <h1> Tirer un jeu au hasard </h1>
+  -->
 
 
   
@@ -90,13 +108,15 @@
     <?php
 include "connDb.php";
 
+
+
 $requete = $db->prepare("SELECT * FROM JEU ORDER BY RAND() LIMIT 1");
 $requete->execute();
 $results = $requete->fetchAll();
 
 // Recuperation du nom et de l'id du jeu tiré
 $id_jeu = $results[0]['id_jeu'];
-$nom_jeu = $results[0]['nom'];
+$nom_jeu = $results[0]['nom_jeu'];
 $prix_jeu = $results[0]['prixConseille'];
 
 // Recuperation des genres du jeu tiré
@@ -116,6 +136,8 @@ echo '<p>Prix : ' . $prix_jeu . '  €</p>';
 $lien = "<form action='pageDuJeu.php?id=" . $id_jeu . "' method='POST' class='description'>  <input type='hidden' name='jeu_id' value='" . $id_jeu . "'><button type='submit'> Page du jeu </button> </form> ";
 echo $lien;
 echo '</div>';
+
+
 
 ?>
 
