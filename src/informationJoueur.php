b8e14a1f0f93230c8446f4c6b7ef8f57fda43686
Nicolas Dussin <ndussin@iutbayonne.univ-pau.fr>
2023-02-10 18:05:58 +0100

    finalisation de l'affichage des informations de l'utilisateur

--- src/informationJoueur.php
+++ src/informationJoueur.php
@@ -18,7 +18,7 @@
             include "connDb.php";
 
             // Récupération de l'identifiant du jeu
-            $id_utilisateur = "U115";
+            $id_utilisateur = "U524";
             
 
             // Préparation de la requête SQL 
@@ -29,17 +29,26 @@
             // Parcoure chaque ligne du résultat et affiche les données du jeu 
             foreach ($results as $row) { 
                 echo '<div class="info_joueur">';
-                echo '<article class="profile_pic_container">';
-                echo '<img src="img/user/' . $row['id_utilisateur'] . '_profil.png" class="profile_pic"';
-                echo '</article>';
-                echo '<article class="details_user">';
+                echo '<div class="profile_pic_container">';
+                $dst='img/user/' . $row['id_utilisateur'] . '_profil.png';
+                if (!file_exists($dst) && !is_dir($dst)) 
+                {
+                    echo '<img src="img/waipt/base_picture_profile.png" class="profile_pic">';    
+                }
+                else 
+                {
+                    echo '<img src="img/user/' . $row['id_utilisateur'] . '_profil.png" class="profile_pic">';
+                }
+                echo '</div>';
+                echo '<div class="details_user">';
                 echo '<h1>' . $row['pseudo'] . '</h1>';
                 echo '<h2> titre </h2>';
-                echo '<p> lvl ' .((round($row['xpQuantite']/1000,0,PHP_ROUND_HALF_UP))-1) . ' :</p>';
+                echo '<p> lvl ' .floor($row['xpQuantite']/1000). ' :</p>';
                 echo '<div class="progress" role="progressbar" aria-label="niveau joueur" aria-valuenow="'.$row['xpQuantite']%1000 .'" aria-valuemin="0" aria-valuemax="1000">';
                 echo '<div class="progress-bar" style="width:'.$row['xpQuantite']%1000/10 .'%">'.$row['xpQuantite']%1000 .'/1000</div>';
                 echo '</div>';
-                echo '</article>';
+                echo '</div>';
+                echo '</div>';
                 echo '</div>';
             }
         ?>
