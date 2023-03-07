ed4667ea27452898e3d7205110217a113eaff3cf
Esteban Labastie <elabastie@iutbayonne.univ-pau.fr>
2023-03-07 13:24:13 +0100

    Creation d'un compte fonctionnel avec securite de doublons

--- src/confirmerCreationCompte.php
+++ src/confirmerCreationCompte.php
@@ -3,43 +3,43 @@
   include "TraiterBD.php";
   include "constantes.php";
 
-  // Attempt insert query execution
-  try {
-    //Recuperation d'un nouvel id d'utilisateur a implementer
-    $traiter = new TraiterBD();
-    $id = $traiter->incrementation_id();
   
-    // Récupération des valeurs des champs de saisie
-    $pseudo = $_POST['identifiant'];
-    $mdp = crypt($_POST['mdp'], $seed);
-    $mail = $_POST['mail'];
-    $dateInscription  = date("Y-m-d");
-    $xpQuantite = 0;
-    $id_utilisateur = $id;
-    $types = 'utilisateur';
 
-    // Préparation de la requête d'insertion
-    $insertion = "INSERT INTO UTILISATEUR (id_utilisateur, pseudo, mdp, mail, types, xpQuantite, dateInscription) VALUES (:id_utilisateur, :pseudo, :mdp, :mail, :types, :xpQuantite, :dateInscription)";
-    $requete = $db->prepare($insertion);
-    
-    // Bind des valeurs
-    $requete->bindParam(':id_utilisateur',$id_utilisateur);
-    $requete->bindParam(':pseudo', $pseudo);
-    $requete->bindParam(':mdp', $mdp);
-    $requete->bindParam(':mail', $mail);
-    $requete->bindParam(':types',$types);
-    $requete->bindParam(':xpQuantite', $xpQuantite);
-    $requete->bindParam(':dateInscription', $dateInscription);
-    
-    // Exécution de la requête
-    $requete->execute();
+  // Récupération des valeurs des champs de saisie
+  $pseudo = $_POST['identifiant'];
+  $mdp = crypt($_POST['mdp'], $seed);
+  $mail = $_POST['mail'];
+  $dateInscription  = date("Y-m-d");
+  $xpQuantite = 0;
+  $types = 'utilisateur';
 
-    // Message de succès
-    echo "Le compte a été créé avec succès.";
+  // Préparation de la requête d'insertion
+  $insertion = "INSERT INTO UTILISATEUR ( pseudo, mdp, mail, types, xpQuantite, dateInscription) VALUES ( :pseudo, :mdp, :mail, :types, :xpQuantite, :dateInscription)";
+  $requete = $db->prepare($insertion);
+  
+  // Bind des valeurs
+  $requete->bindParam(':pseudo', $pseudo);
+  $requete->bindParam(':mdp', $mdp);
+  $requete->bindParam(':mail', $mail);
+  $requete->bindParam(':types',$types);
+  $requete->bindParam(':xpQuantite', $xpQuantite);
+  $requete->bindParam(':dateInscription', $dateInscription);
+  
+  // Exécution de la requête
+  $result = $requete->execute();
+
+  if (!$result) {
     
-  } catch(PDOException $e) {
-    die("ERROR: Could not able to execute $insertion. " . $e->getMessage());
-  }
+    throw new Exception("Erreur : " . $requete->errorInfo()[2]);
+} else {
+    echo 'Compte créé avec succes';
+    
+}
+
+
+    
+    
+    
 
   // Fermeture de la connexion à la base de données
   unset($db);
